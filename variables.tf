078cb1413795fb9b2222471ec8d32ed39b70576d
Riccardo Malesani <169257832+rimalesani@users.noreply.github.com>
2025-10-18 20:46:16 +0200

    Add variables for GCP project and Dataform settings
    Defines variables for GCP project, region, and Dataform configuration.

---
+++ variables.tf
@@ -0,0 +1,38 @@
+# Project variable:
+variable "project_id" {
+  type        = string
+  description = "Your GCP project id"
+}
+
+variable "region" {
+  type        = string
+  description = "Region to deploy resources on"
+  default     = "europe-west1"
+}
+
+# Sink variable:
+variable "log_filter_string" {
+  type        = string
+  description = "Your GA4 property id"
+}
+
+# Cloud Run Function env variables for Dataform run:
+variable "env_var_2" {
+  type        = string
+  description = "Dataform region"
+  sensitive   = false
+}
+
+variable "env_var_3" {
+  type        = string
+  description = "Dataform repo name"
+  sensitive   = false
+  default = "main"
+}
+
+variable "env_var_4" {
+  type        = string
+  description = "Dataform workspace name"
+  sensitive   = false
+  default = "main"
+}
